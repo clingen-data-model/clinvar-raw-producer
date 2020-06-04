70665f524eae5793394c26ea9579154daf8a2363
Lawrence Babb <lbabb@broadinstitute.org>
2020-06-04 17:53:49 -0400

    completed full entity and event type order of processing logic.

--- src/clinvar_scv/core.clj
+++ src/clinvar_scv/core.clj
@@ -31,8 +31,8 @@
    "key.deserializer"                      "org.apache.kafka.common.serialization.StringDeserializer"
    "value.deserializer"                    "org.apache.kafka.common.serialization.StringDeserializer"
    "group.id"                              "dsp_clinvar_drop"
-   "sasl.jaas.config"                      (str "org.apache.kafka.common.security.plain.PlainLoginModule required username=\""
-                                                (:kafka-user opts) "\" password=\"" (:kafka-password opts) "\";")})
+   "sasl.jaas.config"                      (str "org.apache.kafka.common.security.plain.PlainLoginModule required username="\"
+                                                (:kafka-user opts) \"" password="\" (:kafka-password opts) \"";")})
 
 (def file-to-process "files/baseline/clinical_assertion_creates_000000000000")
 (def producer-topic (:kafka-producer-topic app-config))
@@ -41,19 +41,29 @@
 (def project "broad-dsp-monster-clingen-dev")
 (def bucket "broad-dsp-monster-clingen-dev-ingest-results")
 
-(def manifest  ["20200515T153000/clinical_assertion/creates/000000000000"
-                "20200515T153000/clinical_assertion/creates/000000000001"
-                "20200515T153000/clinical_assertion_observation/creates/000000000000"
-                "20200515T153000/gene/creates/000000000000"
-                "20200515T153000/variation/creates/000000000000"
-                "20200515T153000/variation/creates/000000000001"
-                "20200515T153000/variation/creates/000000000002"
-                "20200515T153000/variation/creates/000000000003"
-                "20200515T153000/variation/creates/000000000004"
-                "20200515T153000/gene_association/creates/000000000000"
-                "20200515T153000/variation_archive/creates/000000000000"
-                "20200515T153000/submitter/creates/000000000000"
-                "20200515T153000/submission/creates/000000000000"])
+(def manifest  ["20200515T153000/clinical_assertion/creates/000000000000",
+                "20200515T153000/clinical_assertion/creates/000000000001",
+                "20200515T153000/clinical_assertion_observation/creates/000000000000",
+                "20200515T153000/clinical_assertion_trait/creates/000000000000",
+                "20200515T153000/clinical_assertion_trait_set/creates/000000000000",
+                "20200515T153000/clinical_assertion_variation/creates/000000000000",
+                "20200515T153000/gene/creates/000000000000",
+                "20200515T153000/gene_association/creates/000000000000",
+                "20200515T153000/gene_association/creates/000000000001",
+                "20200515T153000/gene_association/creates/000000000002",
+                "20200515T153000/rcv_accession/creates/000000000000",
+                "20200515T153000/submission/creates/000000000000",
+                "20200515T153000/submitter/creates/000000000000",
+                "20200515T153000/trait/creates/000000000000",
+                "20200515T153000/trait_mapping/creates/000000000000",
+                "20200515T153000/trait_set/creates/000000000000",
+                "20200515T153000/variation/creates/000000000000",
+                "20200515T153000/variation/creates/000000000001",
+                "20200515T153000/variation/creates/000000000002",
+                "20200515T153000/variation/creates/000000000003",
+                "20200515T153000/variation/creates/000000000004",
+                "20200515T153000/variation_archive/creates/000000000000"])
+
 
 (def order-of-processing [{:type "gene"}
                           {:type "variation" :filter {:field :subclass_type :value "SimpleAllele"}}
@@ -61,16 +71,24 @@
                           {:type "variation" :filter {:field :subclass_type :value "Genotype"}}
                           {:type "gene_association"}
                           {:type "variation_archive"}
+                          {:type "trait"}
+                          {:type "trait_set"}
+                          {:type "rcv_accession"}
                           {:type "submitter"}
                           {:type "submission"}
                           {:type "clinical_assertion"}
-                          {:type "clinical_assertion_observation"}])
+                          {:type "clinical_assertion_variation" :filter {:field :subclass_type :value "SimpleAllele"}}
+                          {:type "clinical_assertion_variation" :filter {:field :subclass_type :value "Haplotype"}}
+                          {:type "clinical_assertion_variation" :filter {:field :subclass_type :value "Genotype"}}
+                          {:type "clinical_assertion_trait"}
+                          {:type "clinical_assertion_trait_set"}
+                          {:type "trait_mapping"}])
 
 (def delete-order-of-processing (reverse order-of-processing))
 
-(def operation {:create {:order order-of-processing}
-                :update {:order order-of-processing}
-                :delete {:order delete-order-of-processing}})
+(def event-procedures [{:event-type :create :order order-of-processing :filter-string "creates"}
+                       {:event-type :update :order order-of-processing  :filter-string "updates"}
+                       {:event-type :delete :order delete-order-of-processing :filter-string "deletes"}])
 
 (def gc-storage (.getService (StorageOptions/getDefaultInstance)))
 
@@ -84,17 +102,24 @@
   (println filter)
   (let [blob-id (BlobId/of bucket file)
         blob (.get gc-storage blob-id)]
-    (with-open [rdr (-> blob (.reader (make-array Blob$BlobSourceOption 0)) (Channels/newReader "UTF-8") BufferedReader.) ]
-      (let [lines (map #(assoc (json/parse-string % true) :type entity-type) (line-seq rdr))
-            records (if filter-field
-                      (filter #(= (:value filter-field) (get % (:field filter-field))) lines)
-                      lines)]
-        (doseq [content (take 5 records)]
-          (let [key (str (:id content) "_" datetime)
-                event {:time datetime :type event-type :content content}]
-            (println content)
-            (send-update-to-exchange producer topic {:key key
-                                                     :value (json/generate-string event)})))))))
+    (println file)
+    (if blob
+      (with-open [rdr (-> blob (.reader (make-array Blob$BlobSourceOption 0)) (Channels/newReader "UTF-8") BufferedReader.) ]
+        (let [lines (map #(assoc (json/parse-string % true) :type entity-type) (line-seq rdr))
+              records (if filter-field
+                        (filter #(= (:value filter-field) (get % (:field filter-field))) lines)
+                        lines)]
+          (doseq [content records]
+            (let [key (str (:id content) "_" datetime)
+                  event {:time datetime :type event-type :content content}]
+              (println content)
+              (send-update-to-exchange producer topic {:key key
+                                                       :value (json/generate-string event)})))))
+      (println "file not found"))))
+
+(defn filter-files
+  [filter-string files]
+  (filter #(re-find (re-pattern (str "/" filter-string "/")) % ) files ))
 
 (defn process-clinvar-drop
   "parses and processes the clinvar drop notification from dsp."
@@ -102,21 +127,25 @@
 
   ; 1. parse the drop message to determine where the files are
   ; this will return the folder and bucket and file manifest
+  ;... pull out manifest here
+
+  ; need to verify all entries in manifest are processed else warning and logging on unknown files.
+
 
   ; 2. process the folder structure in order of tables for create-update and then reverse for deletes
+  (doseq [procedure event-procedures]
+    (let [files (filter-files (:filter-string procedure) manifest)]
 
-  ; forward order of processing loop
-  (doseq [record-type order-of-processing
-          file (filter #(re-find (re-pattern (str "/" (:type record-type) "/")) % ) manifest )]
-    (let [[_ entity-type event-type _] (s/split file #"/")]
+    (doseq [record-type (:order procedure)
+            file (filter-files (:type record-type) files )]
       (process-clinvar-drop-file {:producer producer
                                   :topic topic
                                   :bucket bucket
                                   :file file
-                                  :entity-type entity-type
+                                  :entity-type (:type record-type)
                                   :datetime release-date
-                                  :event-type event-type
-                                  :filter-field (:filter record-type)}))))
+                                  :event-type (:event-type procedure)
+                                  :filter-field (:filter record-type)})))))
 
 
 (defn listen-for-clinvar-drop
