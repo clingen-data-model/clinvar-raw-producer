ffecd7716787f6408ccd3ce0f0faf6a8f7b1bd03
Kyle Ferriter <kferrite@broadinstitute.org>
2020-06-11 16:26:52 -0400

    Refactor config to config.clj. Add timbre to project. Begin mocking

--- src/clinvar_scv/core.clj
+++ src/clinvar_scv/core.clj
@@ -4,165 +4,102 @@
             [jackdaw.client.log :as jl]
             [clojure.java.io :as io]
             [cheshire.core :as json]
-            [clojure.string :as s])
-  (:import [com.google.cloud.storage StorageOptions BlobId]
+            [clojure.string :as s]
+            [clojure.core :as core]
+            [clinvar-scv.config :as cfg]
+            [taoensso.timbre :as timbre
+             :refer [log trace debug info warn error fatal report
+                     logf tracef debugf infof warnf errorf fatalf reportf
+                     spy get-env]])
+  (:import [com.google.cloud.storage Storage StorageOptions BlobId Blob]
            com.google.cloud.storage.Blob$BlobSourceOption
            java.nio.channels.Channels
-           java.io.BufferedReader)
+           java.io.BufferedReader
+           (java.util Date TimeZone)
+           (java.text SimpleDateFormat))
   (:gen-class))
 
-(def app-config {:kafka-host     "pkc-4yyd6.us-east1.gcp.confluent.cloud:9092"
-                 :kafka-user     (System/getenv "KAFKA_USER")
-                 :kafka-password (System/getenv "KAFKA_PASSWORD")
-                 :kafka-producer-topic    "clinvar-raw"
-                 :kafka-consumer-topic    "broad_dsp_clinvar"})
 
-(defn kafka-config
-  "Expects, at a minimum, :user and :password in opts. "
-  [opts]
-  {"ssl.endpoint.identification.algorithm" "https"
-   "compression.type"                      "gzip"
-   "sasl.mechanism"                        "PLAIN"
-   "request.timeout.ms"                    "20000"
-   "bootstrap.servers"                     (:kafka-host opts)
-   "retry.backoff.ms"                      "500"
-   "security.protocol"                     "SASL_SSL"
-   "key.serializer"                        "org.apache.kafka.common.serialization.StringSerializer"
-   "value.serializer"                      "org.apache.kafka.common.serialization.StringSerializer"
-   "key.deserializer"                      "org.apache.kafka.common.serialization.StringDeserializer"
-   "value.deserializer"                    "org.apache.kafka.common.serialization.StringDeserializer"
-   "group.id"                              "dsp_clinvar_producer"
-   "sasl.jaas.config"                      (str "org.apache.kafka.common.security.plain.PlainLoginModule required username="\"
-                                                (:kafka-user opts) \"" password="\" (:kafka-password opts) \"";")})
+;(def file-to-process "files/baseline/clinical_assertion_creates_000000000000")
+;(def producer-topic (:kafka-producer-topic cfg/app-config))
+;(def release-date "2020-05-21T20:02:00Z")
+(defn get-release-date []
+  (let [now (Date.)
+        fmt (SimpleDateFormat. cfg/date-format)]
+  (.setTimeZone fmt (TimeZone/getTimeZone "UTC"))
+  (.format fmt now)))
 
-(def file-to-process "files/baseline/clinical_assertion_creates_000000000000")
-(def producer-topic (:kafka-producer-topic app-config))
-(def release-date "2020-05-21T20:02:00Z")
-
-(def project "broad-dsp-monster-clingen-dev")
+;(def project "broad-dsp-monster-clingen-dev")
 (def bucket "broad-dsp-monster-clingen-dev-ingest-results")
 
-(def manifest  ["20200515T153000/clinical_assertion/creates/000000000000",
-                "20200515T153000/clinical_assertion/creates/000000000001",
-                "20200515T153000/clinical_assertion_observation/creates/000000000000",
-                "20200515T153000/clinical_assertion_trait/creates/000000000000",
-                "20200515T153000/clinical_assertion_trait_set/creates/000000000000",
-                "20200515T153000/clinical_assertion_variation/creates/000000000000",
-                "20200515T153000/gene/creates/000000000000",
-                "20200515T153000/gene_association/creates/000000000000",
-                "20200515T153000/gene_association/creates/000000000001",
-                "20200515T153000/gene_association/creates/000000000002",
-                "20200515T153000/rcv_accession/creates/000000000000",
-                "20200515T153000/submission/creates/000000000000",
-                "20200515T153000/submitter/creates/000000000000",
-                "20200515T153000/trait/creates/000000000000",
-                "20200515T153000/trait_mapping/creates/000000000000",
-                "20200515T153000/trait_set/creates/000000000000",
-                "20200515T153000/variation/creates/000000000000",
-                "20200515T153000/variation/creates/000000000001",
-                "20200515T153000/variation/creates/000000000002",
-                "20200515T153000/variation/creates/000000000003",
-                "20200515T153000/variation/creates/000000000004",
-                "20200515T153000/variation_archive/creates/000000000000"])
-
-
-(def order-of-processing [{:type "gene"}
-                          {:type "variation" :filter {:field :subclass_type :value "SimpleAllele"}}
-                          {:type "variation" :filter {:field :subclass_type :value "Haplotype"}}
-                          {:type "variation" :filter {:field :subclass_type :value "Genotype"}}
-                          {:type "gene_association"}
-                          {:type "variation_archive"}
-                          {:type "trait"}
-                          {:type "trait_set"}
-                          {:type "rcv_accession"}
-                          {:type "submitter"}
-                          {:type "submission"}
-                          {:type "clinical_assertion"}
-                          {:type "clinical_assertion_variation" :filter {:field :subclass_type :value "SimpleAllele"}}
-                          {:type "clinical_assertion_variation" :filter {:field :subclass_type :value "Haplotype"}}
-                          {:type "clinical_assertion_variation" :filter {:field :subclass_type :value "Genotype"}}
-                          {:type "clinical_assertion_trait"}
-                          {:type "clinical_assertion_trait_set"}
-                          {:type "trait_mapping"}])
-
-(def delete-order-of-processing (reverse order-of-processing))
-
-(def event-procedures [{:event-type :create :order order-of-processing :filter-string "created"}
-                       {:event-type :update :order order-of-processing  :filter-string "updated"}
-                       {:event-type :delete :order delete-order-of-processing :filter-string "deleted"}])
+; Example manifest vector
+;(def manifest  ["20200515T153000/clinical_assertion/creates/000000000000"
+;                "20200515T153000/clinical_assertion/creates/000000000001"
+;                "20200515T153000/clinical_assertion_observation/creates/000000000000"])
 
 (def gc-storage (.getService (StorageOptions/getDefaultInstance)))
 
 (defn send-update-to-exchange [producer topic {:keys [key value]}]
+  (tracef "Sending message to topic %s: %s:%s" topic key value)
+  ;(println "sending message: " key)
   (jc/send! producer (jd/->ProducerRecord {:topic-name topic} key value)))
 
 (defn process-clinvar-drop-file
   "return a seq of parsed json messages"
-  [{:keys [producer topic bucket file entity-type datetime event-type filter-field]}]
+  [{:keys [producer topic bucket file entity-type datetime event-type]}]
+  (debugf "Processing dropped file %s for entity-type %s and event-type %s"
+          file entity-type event-type)
   (let [blob-id (BlobId/of bucket file)
         blob (.get gc-storage blob-id)]
-    (println file)
-    (if blob
-      (with-open [rdr (-> blob (.reader (make-array Blob$BlobSourceOption 0)) (Channels/newReader "UTF-8") BufferedReader.) ]
-        (let [lines (map #(assoc (json/parse-string % true) :type entity-type) (line-seq rdr))
-              records (if filter-field
-                        (filter #(= (:value filter-field) (get % (:field filter-field))) lines)
-                        lines)]
-          (doseq [content records]
-            (let [key (str (:id content) "_" datetime)
-                  event {:time datetime :type event-type :content content}]
-              (send-update-to-exchange producer topic {:key key
-                                                       :value (json/generate-string event)})))))
-      (println "file not found"))))
+    (tracef "Opening reader to blob %s/%s" bucket file)
+    (with-open [rdr (-> blob (.reader (make-array Blob$BlobSourceOption 0)) (Channels/newReader "UTF-8") BufferedReader.) ]
+      (let [lines (line-seq rdr)]
+        (doseq [line (take 5 lines)]
+          (tracef "Parsing file line %s" line)
+          (let [content (assoc (json/parse-string line true) :type entity-type)
+                key (str (:id content) "_" datetime)
+                event {:time datetime :type event-type :content content}]
 
-(defn filter-files
-  [filter-string files]
-  (filter #(re-find (re-pattern (str "/" filter-string "/")) % ) files ))
+            (send-update-to-exchange producer topic {:key   key
+                                                     :value (json/generate-string event)})))))))
 
 (defn process-clinvar-drop
   "parses and processes the clinvar drop notification from dsp."
-  [producer topic msg]
+  [producer producer-topic file-listing]
 
   ; 1. parse the drop message to determine where the files are
   ; this will return the folder and bucket and file manifest
-  ;... pull out manifest here
+  (debugf "File listing: %s" (s/join "," file-listing))
+  ; 2. process the folder structure in order of tables for create-update and then reverse for deletes
 
-  (let [parsed-drop-record (json/parse-string msg true)]
-
-    ;; need to verify all entries in manifest are processed else warning and logging on unknown files.
-
-
-    ;; 2. process the folder structure in order of tables for create-update and then reverse for deletes
-    (doseq [procedure event-procedures]
-      (let [files (filter-files (:filter-string procedure) (:files parsed-drop-record))]
-
-        (doseq [record-type (:order procedure)
-                file (filter-files (:type record-type) files )]
-          (process-clinvar-drop-file {:producer producer
-                                      :topic topic
-                                      :bucket bucket
-                                      :file file
-                                      :entity-type (:type record-type)
-                                      :datetime release-date
-                                      :event-type (:event-type procedure)
-                                      :filter-field (:filter record-type)}))))))
-
-
-(def listen-for-drop (atom true))
+    ; 2a. assume we have bucket and manifest (vector of files to process with full paths in bucket.
+  (doseq [file file-listing]
+    (let [[_ entity-type event-type _] (s/split file #"/")]
+      (process-clinvar-drop-file {:producer producer
+                                  :topic producer-topic
+                                  :bucket bucket
+                                  :file file
+                                  :entity-type entity-type
+                                  :datetime get-release-date
+                                  :event-type event-type}))))
 
 (defn listen-for-clinvar-drop
-  "listens to consumer topic for dsp clinvar drop notifications."
+  "Listens to consumer topic for dsp clinvar drop notifications in `manifest` file list format.
+  Produces output messages on producer topic."
   [opts]
-  (println "listening for clinvar drop")
-  (with-open [consumer (jc/consumer (kafka-config opts))
-              producer (jc/producer (kafka-config opts))]
+  (debugf "Opening consumer and producer kafka streams")
+  (with-open [consumer (jc/consumer (cfg/kafka-config opts))
+              producer (jc/producer (cfg/kafka-config opts))]
+    (debugf "Subscribing to consumer topic %s" (:kafka-consumer-topic opts))
     (jc/subscribe consumer [{:topic-name (:kafka-consumer-topic opts)}])
-    (while @listen-for-drop
+    (while true
       (let [msgs (jc/poll consumer 100)]
         (doseq [m msgs]
-          (println m)
+          (infof "Received message %s\n" m)
+          ; The message contents is within the ConsumerRecord as :value
+          ; The value is just a list of file paths, no additional processing needed here
           (process-clinvar-drop producer (:kafka-producer-topic opts) (:value m)))))))
 
 (defn -main
   [& args]
-  (listen-for-clinvar-drop app-config))
+  (listen-for-clinvar-drop cfg/app-config))
--- test/clinvar_scv/core_test.clj
+++ test/clinvar_scv/core_test.clj
@@ -1,7 +1,63 @@
 (ns clinvar-scv.core-test
   (:require [clojure.test :refer :all]
-            [clinvar-scv.core :refer :all]))
+            [clinvar-scv.core :refer :all]
+            [jackdaw.data :as jd]
+            [jackdaw.client :as jc]
+            [jackdaw.streams.mock :as jsm]
+            [jackdaw.serdes.json :as jdj]
+            [cheshire.core :as json]))
 
-(deftest a-test
-  (testing "FIXME, I fail."
-    (is (= 0 1))))
+
+(deftest bucket-test
+  (testing "Ensure bucket is set to correct value"
+    (is (= "broad-dsp-monster-clingen-dev-ingest-results" bucket))))
+
+(deftest validate-datetime
+  (testing "Validate datetime format"
+    ; Replace with regex validating of message datetime field?
+    (is (= 1 1))))
+
+(defn mock-producer [topic-name]
+  (let [test-driver jsm/build-driver
+        producer (jsm/producer test-driver {:keys [topic-name jdj/serializer jdj/serializer]})]
+    (producer)))
+
+(defn consume [topic-name]
+  (let [test-driver jsm/build-driver
+        ;consumer (jsm/consumer test-driver {:keys [topic-name :key json/generate-string]})]
+        ]
+    (jsm/consume test-driver {:keys [topic-name jdj/serializer jdj/serializer]})))
+
+(deftest test-send-update
+  (testing "Test sending message to output producer"
+    (let [producer mock-producer]
+
+
+      )
+))
+
+;(deftest test-send-update
+;  (testing "Test proper message to output producer"
+;    (let [expected-key "1"
+;          datetime get-release-date
+;          topic "clinvar-scv-test"
+;          event-type "Created"
+;          event {:TestKey "TestValue"} ;"{\"TestKey\": \"TestValue\"}"
+;          expected-value {:time datetime :type event-type :content (json/generate-string event)}]
+;
+;      (letfn [
+;              (producer [{:keys [topic-name]} key value]
+;                (is (= key expected-key))
+;                (is (= value expected-value))
+;                (is (= topic-name topic))
+;                )
+;              ]
+;      (with-redefs-fn {#'send-update-to-exchange
+;                       (fn [producer topic {:keys [key value]}]
+;                         ;(tracef "Sending message to topic %s: %s:%s" topic key value)
+;                         ;(println "sending message: " key)
+;                         ;(jc/send! producer (jd/->ProducerRecord {:topic-name topic} key value)))
+;                         (producer {:topic-name topic} key value))}
+;        (send-update-to-exchange producer topic {:key   key
+;                                                 :value (json/generate-string event)}))))))
+
